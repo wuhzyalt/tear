d081b3ecd5b14a14251ca17cc735b58e1218fc5c
wuhzyalt <140111389+wuhzyalt@users.noreply.github.com>
2023-07-20 17:14:48 -0400

    Update portfolio.js

--- home/javascript/portfolio.js
+++ home/javascript/portfolio.js
@@ -28,7 +28,7 @@ $(document).ready(() => {
   for (let i in links) {
     let link = links[i];
 
-    $('#marquee').append(`<a href="https://swazypooh.netlify.app/${link.link}" target="_BLANK">${link.name}</a>`);
+    $('#marquee').append(`<a href="https://bury.lol/${link.link}" target="_BLANK">${link.name}</a>`);
 
     link = $('#marquee').children('a').last();
 
